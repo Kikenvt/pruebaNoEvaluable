cfff944e6c54740095657a43344a8b94d6a9e616
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-10-03 10:46:42 +0200

    Añadido readme

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Enrique Fernandez-Campoamor Fernandez
+// https://github.com/Kikenvt/pruebaNoEvaluable.git
 
 
-let numero = prompt("Ingrese un número: ")
+let numero = parseFloat(prompt("Ingrese un número: "))
+
+// Añadida la funcion eval en caso de que el usuario ingrese un operador.
 
 if (eval(numero) > 0) {
   console.log("El número ingresado es positivo")
@@ -9,7 +12,10 @@ if (eval(numero) > 0) {
 } else if (eval(numero) < 0) {
   console.log("El número ingresado es negativo")
   alert("El número ingresado es negativo")
-} else {
+} else if(eval(numero)=0){
   console.log("El número ingresado es igual a cero.")
   alert("El número ingresado es igual a cero.")
+}else{
+    console.log("Dato no válido")
+    alert("Dato no válido")
 }
